48169c66e13edac9955d8d06d32a5e833283b9fb
GNicolasgomez <gnicolasgomez@gmail.com>
2024-12-06 12:18:00 -0300

    config db datastore in production

--- config/env/production.js
+++ config/env/production.js
@@ -67,7 +67,7 @@ module.exports = {
        * https://sailsjs.com/config/datastores                                     *
        *                                                                           *
        ****************************************************************************/
-      // ssl: { rejectUnauthorized: false },
+      ssl: true,
     },
   },
 
@@ -169,7 +169,7 @@ module.exports = {
      * > (For a full list, see https://sailsjs.com/plugins/sessions)            *
      *                                                                          *
      ***************************************************************************/
-    // adapter: '@sailshq/connect-redis',
+    adapter: '@sailshq/connect-redis',
     // url: 'redis://user:password@localhost:6379/databasenumber',
     //--------------------------------------------------------------------------
     // /\   OR, to avoid checking it in to version control, you might opt to
