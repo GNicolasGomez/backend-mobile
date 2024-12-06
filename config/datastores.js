9d3de2606b19470adb38ec6a488accbd600dd8ff
GNicolasgomez <gnicolasgomez@gmail.com>
2024-12-06 13:17:11 -0300

    configuration

--- config/datastores.js
+++ config/datastores.js
@@ -48,6 +48,6 @@ module.exports.datastores = {
      ***************************************************************************/
     adapter: "sails-postgresql",
     url: process.env.DATABASE_URL,
-    ssl:true
+    // ssl:true
   },
 };
